52dc8b25ffe6aae679de23522b0087fe3cc912e0
Mounika <pinkymounika414@gmail.com>
2023-05-18 12:47:56 +0530

    if-elif statement in for loop with greetings string

---
+++ 202N1A0569.py
@@ -0,0 +1,8 @@
+u = int(input("Enter your Number :"))
+for i in range(0,5):
+    if u > 10:
+        print("welcome back!")
+    elif u < 10:
+        print("go back")
+    else:
+        print("maybe think about yourself")        
